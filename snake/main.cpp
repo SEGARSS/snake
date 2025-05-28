7536795d9bb6167f3d5a670d3950654d716db073
SEGARSS <vim_branet@mail.ru>
2025-05-28 13:08:53 +0500

    рисуем фигуры

--- snake/main.cpp
+++ snake/main.cpp
@@ -20,47 +20,47 @@ int main()
     //rectangle.setPosition({ 30, 40 }); // Позиция
 
     //Хвост
-    CircleShape circle1(15);//Фигура (размер)
-    circle1.setOutlineColor(Color(171, 72, 39)); // Цвет линии обводки фигуры
-    circle1.setOutlineThickness(5); // Толщина линии обводки фигуры
-    circle1.setFillColor(Color::Red); // Цвет заливки фигуры
-    circle1.setPosition({ 100, 100 }); // Позиция
+    CircleShape circle1(15);                     //Фигура (размер)
+    circle1.setOutlineColor(Color(171, 72, 39));// Цвет линии обводки фигуры
+    circle1.setOutlineThickness(5);            // Толщина линии обводки фигуры
+    circle1.setFillColor(Color::Red);         // Цвет заливки фигуры
+    circle1.setPosition({ 100, 100 });       // Позиция
 
     //Тело
-    CircleShape circle2(15); //Фигура (размер)
-    circle2.setOutlineColor(Color(171, 72, 39)); // Цвет линии обводки фигуры
-    circle2.setOutlineThickness(5); // Толщина линии обводки фигуры
-    circle2.setFillColor(Color::Red); // Цвет заливки фигуры
-    circle2.setPosition({ 140, 100 }); // Позиция
+    CircleShape circle2(15);                     //Фигура (размер)
+    circle2.setOutlineColor(Color(171, 72, 39));// Цвет линии обводки фигуры
+    circle2.setOutlineThickness(5);            // Толщина линии обводки фигуры
+    circle2.setFillColor(Color::Red);         // Цвет заливки фигуры
+    circle2.setPosition({ 140, 100 });       // Позиция
 
     //Голова
-    CircleShape circle3(15); //Фигура (размер)
-    circle3.setOutlineColor(Color(171, 72, 39)); // Цвет линии обводки фигуры
-    circle3.setOutlineThickness(5); // Толщина линии обводки фигуры
-    circle3.setFillColor(Color::Red); // Цвет заливки фигуры
-    circle3.setPosition({ 180, 100 }); // Позиция
+    CircleShape circle3(15);                     //Фигура (размер)
+    circle3.setOutlineColor(Color(171, 72, 39));// Цвет линии обводки фигуры
+    circle3.setOutlineThickness(5);            // Толщина линии обводки фигуры
+    circle3.setFillColor(Color::Red);         // Цвет заливки фигуры
+    circle3.setPosition({ 180, 100 });       // Позиция
 
     //Верхний глаз
-    CircleShape circle4(3); //Фигура (размер)
-    circle4.setOutlineColor(Color::Black); // Цвет линии обводки фигуры
-    circle4.setOutlineThickness(1); // Толщина линии обводки фигуры
+    CircleShape circle4(3);                //Фигура (размер)
+    circle4.setOutlineColor(Color::Black);// Цвет линии обводки фигуры
+    circle4.setOutlineThickness(1);      // Толщина линии обводки фигуры
     circle4.setFillColor(Color::Black); // Цвет заливки фигуры
     circle4.setPosition({ 197, 118 }); // Позиция
 
     //Нижний глаз
-    CircleShape circle5(3); //Фигура (размер)
-    circle5.setOutlineColor(Color::Black); // Цвет линии обводки фигуры
-    circle5.setOutlineThickness(1); // Толщина линии обводки фигуры
+    CircleShape circle5(3);                //Фигура (размер)
+    circle5.setOutlineColor(Color::Black);// Цвет линии обводки фигуры
+    circle5.setOutlineThickness(1);      // Толщина линии обводки фигуры
     circle5.setFillColor(Color::Black); // Цвет заливки фигуры
     circle5.setPosition({ 197, 106 }); // Позиция
 
     //Еда
     CircleShape octagon(80, 8);
-    octagon.setRadius(10); //Размер фигуры
-    octagon.setOutlineColor(Color::Black); // Цвет линии обводки фигуры
-    octagon.setOutlineThickness(1); // Толщина линии обводки фигуры
+    octagon.setRadius(10);                 //Размер фигуры
+    octagon.setOutlineColor(Color::Black);// Цвет линии обводки фигуры
+    octagon.setOutlineThickness(1);      // Толщина линии обводки фигуры
     octagon.setFillColor(Color::Green); // Цвет заливки фигуры
-    octagon.setPosition({ 220, 100 }); // Позиция
+    octagon.setPosition({ 240, 105 }); // Позиция
 
 
     // Старт игрового окна
