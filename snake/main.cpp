f08aa698f6977adde65448f82824e8065a46ee21
SEGARSS <vim_branet@mail.ru>
2025-05-28 10:10:20 +0500

    настройка проекта

---
+++ snake/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Размер игрового окна
+    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML window");
+
+    // Старт игрового окна
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.display();
+    }
+}
