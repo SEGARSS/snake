587cebad1c8bc449ea46c93361fbb80f7e6c4f54
SEGARS <Vim_Branet@mail.ru>
2025-06-16 16:15:17 +0500

    работа дома

--- snake/main.cpp
+++ snake/main.cpp
@@ -226,7 +226,7 @@ int main()
             snakeBody.back().setPosition(pos);
         }
 
-        Vector2f pos2 = food.getPosition();
+        
 
         //дорабоать
         if (Vector2f(6.0 + 50 + 12, 6.0 + 50 + 12) == snakeBody.back().getPosition())
